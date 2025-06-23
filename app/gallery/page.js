1bdac7891f99461b7a55385c95fed067f317bd05
Zasha <zahid.shaikh@live.com>
2025-06-23 14:16:56 +0530

    Added Gallery Images

--- app/gallery/page.js
+++ app/gallery/page.js
@@ -11,7 +11,36 @@ const page = () => {
       <section className="team-page-area pt-130 rpt-100 pb-100 rpb-70 rel z-1">
         <div className="container">
           <div className="row align-items-center">
-           
+            <div className="col-4">
+              <img className="img-fluid" src='/1.jpeg' alt="image"/>
+            </div>
+            <div className="col-4">
+              <img className="img-fluid" src='/2.jpeg' alt="image"/>
+            </div>
+            <div className="col-4">
+              <img className="img-fluid" src='/3.jpeg' alt="image"/>
+            </div>
+            <div className="col-4">
+              <img className="img-fluid" src='/4.jpeg' alt="image"/>
+            </div>
+            <div className="col-4">
+              <img className="img-fluid" src='/5.jpeg' alt="image"/>
+            </div>
+            <div className="col-4">
+              <img className="img-fluid" src='/6.jpeg' alt="image"/>
+            </div>
+            <div className="col-4">
+              <img className="img-fluid" src='/7.jpeg' alt="image"/>
+            </div>
+            <div className="col-4">
+              <img className="img-fluid" src='/8.jpeg' alt="image"/>
+            </div>
+            <div className="col-4">
+              <img className="img-fluid" src='/9.jpeg' alt="image"/>
+            </div>
+            <div className="col-4">
+              <img className="img-fluid" src='/10.jpeg' alt="image"/>
+            </div>
           </div>
         </div>
       </section>
